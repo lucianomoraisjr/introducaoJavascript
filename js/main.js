e8a495557b31e8c62575c246a8c607ad0b8d209a
unknown <lucianomoraisjr@gmail.com>
2020-06-29 21:15:29 -0300

    indrodução ao JS

---
+++ js/main.js
@@ -0,0 +1,39 @@
+//alert("Bem Vindo");
+nome = "luciano junior"
+Frase="real madrid e o melhor time do mundo"
+console.log(nome)
+console.log(Frase.toLowerCase())//deixa tudo em minusculo
+console.log(Frase.toUpperCase())//deixa em maiusculo 
+console.log(Frase.replace("real madrid", "Vila Nova"))// troca real madrid por vila nova 
+ var lista = ["maça", "pera", "laranja"];
+ lista.pop()
+ lista.push("uva")
+ console.log(lista);
+ console.log(lista.reverse())
+ console.log(lista.toString());
+ console.log(lista.join("-"))
+ var idade = prompt("Qual e a sua idade ?")
+ 
+ if(idade>=18){
+     alert("maior de idade")
+ }else{
+     alert("menor de idade")
+ }
+ 
+  var count =0;
+  while(count < 10){
+  console.log(count)
+  count++;
+  };
+  var i;
+  for (i=0; i<=15;i++){
+      console.log(i);
+  }
+
+  var d =new Date();
+  var lista = [“Alemanha”, “Inglaterra”, “Escócia”]/
+  lista.push(“Polônia”);
+  lista.pop(“Inglaterra”);
+  console.log(lista)
+  
+
